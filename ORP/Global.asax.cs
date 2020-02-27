30b2ea697093bd18e335b994d4fe8f94ca05647d
madc <madc@netcompany.com>
2020-02-27 13:17:14 +0100

    Added Users and Clearance + created connection between database and EF

--- ORP/Global.asax.cs
+++ ORP/Global.asax.cs
@@ -16,6 +16,6 @@ namespace ORP
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
-		}
+        }
 	}
 }
---
+++ ORP/TestRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ORP.Model;
+
+namespace ORP
+{
+    public class TestRepository
+    {
+        private string connectionString =
+            "Data Source=dbs-oapl.database.windows.net;Initial Catalog=db-prod;User ID=dbs-oapl;Password=oceanicFlyAway16";
+
+        public bool AddUser()
+        {
+            using (var context = new OrpContext())
+            {
+                context.Users.Add(new User
+                {
+                    Name = "Mads",
+                    Clearance = new Clearance
+                    {
+                        Level = 1,
+                        Name = "CIA"
+                    },
+                    Password = "password1"
+                });
+                context.SaveChanges();
+            }
+
+            return true;
+
+        }
+    }
+}
