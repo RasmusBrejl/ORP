975c53e0521361a1574ec03f40d2441b2f8a7369
nc-ritr <ritr@netcompany.com>
2020-02-27 13:38:16 +0100

    Added controllers

---
+++ ORP/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ORP.Model;
+using ORP.Models;
+
+namespace ORP.Controllers
+{
+    public class OrderController : Controller
+    {
+        public Order[] GetAllOrders(User user)
+        {
+            //Dummy
+            Order[] orders = null;
+            return orders;
+
+        }
+
+        public bool AddOrder(Order order)
+        {
+            //Dummy
+            return true;
+        }
+
+        public Order GetOrder(int OrderId)
+        {
+            //Dummy
+            Order order = null;
+            return order;
+        }
+    }
+}
