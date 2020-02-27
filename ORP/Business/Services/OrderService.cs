f31da0423fe6bc4a29fd694171a1c05befa03020
Ivan Kjær Gammeljord <ikg@netcompany.com>
2020-02-27 14:27:50 +0100

    Added Services User/Order
    Signed-off-by: Ivan Kjær Gammeljord <ikg@netcompany.com>

---
+++ ORP/Business/Services/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using ORP.Models;
+
+namespace ORP.Buisness.Services
+{
+    public class OrderService
+    {
+        private readonly TestRepository _orderRepository;
+
+        public OrderService(TestRepository orderRepository)
+        {
+            // TODO : Replace TestRepository with OrderRepository
+            _orderRepository = orderRepository;
+        }
+
+        public Order[] GetAllOrders(User user) 
+        {
+            // TODO add getter
+            return new Order[1];
+        }
+
+        public bool AddOrder(Order order)
+        {
+            // TODO add setter
+            return true;
+        }
+
+        public Order GetOrder(int OrderId)
+        {
+            // TODO add getter
+            return new Order();
+        }
+    }
+}
---
+++ ORP/Business/Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿using ORP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ORP.Buisness.Services
+{
+    public class UserService
+    {
+        // private readonly UserRepository _userRepository;
+
+        // public UserService(UserRepository userRepository)
+        public UserService()
+        {
+            // _userRepository = userRepository;
+        }
+
+        public bool UserHasClearance(User user, String ClearanceName)
+        {
+            return user.Clearance.Name.Equals(ClearanceName);
+        }
+
+        public bool TryLogIn(User user, out string errMsg)
+        {
+            errMsg = "";
+            if (true)
+            {
+                return true;
+            }
+            errMsg = "Error during login attempt";
+            return false;
+        }
+    }
+}
