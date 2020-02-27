7c5b3b88d54fc8e6158587386e744421ef7b1d8b
Rasmus Kongsmar Brejl <rkb@netcompany.com>
2020-02-27 12:49:20 +0100

    Added C# classes for our domain

---
+++ ORP/Models/ParcelCategory.cs
@@ -0,0 +1,11 @@
+﻿using ORP.Models.Enums;
+
+namespace ORP.Models
+{
+	public class ParcelCategory
+	{
+		public int ParcelCategoryId { get; set; }
+		public ParcelType ParcelType { get; set; }
+		public float PriceModifier { get; set; }
+	}
+}
