fdad6e2efb2add45abfe0bbe05b68c0e81cad7f7
nc-ritr <ritr@netcompany.com>
2020-02-27 20:36:20 +0100

    New migration added columns to Cities

---
+++ ORP/Migrations/202002271927293_Added more columns to city.cs
@@ -0,0 +1,20 @@
+﻿namespace ORP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Addedmorecolumnstocity : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Cities", "Valid", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Cities", "PricePenalty", c => c.Single(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Cities", "PricePenalty");
+            DropColumn("dbo.Cities", "Valid");
+        }
+    }
+}
